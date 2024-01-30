0a8f7bca74c7ae8e8d3f6c73d17739ea5769f581
Cristian <cristian91493@gmail.com>
2024-01-30 14:09:11 -0500

    got the login backend working

--- backend/server.js
+++ backend/server.js
@@ -19,12 +19,19 @@ const PORT = 8080
 app.use(cors())
 app.use(express.json())
 
+// Test
+app.get('/api/test', (req,res) => {
+    console.log('test')
+    res.json('Hello World!')
+})
+
 
 // login point
 app.use('/auth', authRoutes)
 // add middleware before route, this also grabs req.body/params
 // you could also put them inside specific routes inside the routes folder
-// app.use('/api/users', authorize, userRoutes)
+
+app.use('/api/users', authorize, userRoutes)
 
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT)
