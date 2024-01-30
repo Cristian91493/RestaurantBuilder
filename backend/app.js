d47f59671cba43bd9ab48aece3ccc8ae6f29a85a
Cristian <cristian91493@gmail.com>
2024-01-30 13:09:45 -0500

    added db connection

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 const express = require("express")
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./db');
+const apiRoutes = require('./routes/api'); // Import the API routes
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -12,6 +18,10 @@ app.use(cors());
 
 //Connect to MongoDb
 connectDB();
+
+
+//Use API routes
+app.use('/api', apiRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
