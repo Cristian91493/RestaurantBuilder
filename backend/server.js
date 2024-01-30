1b553677c242f2ed2838960a5c83db39fd3fb212
Cristian <cristian91493@gmail.com>
2024-01-30 13:42:50 -0500

    added auth and backend

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const cors = require('cors')
+
+require('dotenv').config()
+
+const mongoConfig = require('./config')
+
+mongoConfig()
+
+const authRoutes = require('./routes/authRoutes')
+const userRoutes = require('./routes/userRoutes')
+// middleware
+const { authorize } = require('./middleware/authMiddleware')
+
+const app = express()
+
+const PORT = 8080
+
+app.use(cors())
+app.use(express.json())
+
+
+// login point
+app.use('/auth', authRoutes)
+// add middleware before route, this also grabs req.body/params
+// you could also put them inside specific routes inside the routes folder
+// app.use('/api/users', authorize, userRoutes)
+
+app.listen(PORT, () => {
+    console.log('Listening on port: ' + PORT)
+})
