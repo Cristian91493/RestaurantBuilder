10c1c76a9b41e05e722acb28b2ab235cada5f8d8
Cristian <cristian91493@gmail.com>
2024-01-30 14:23:03 -0500

    added front end of registration

--- client/restaurantBuilder/src/App.jsx
+++ client/restaurantBuilder/src/App.jsx
@@ -1,35 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+
+import { useState, useEffect } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import Register from "./pages/Register";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState({});
+  // we need time to check if token is loaded in to state, not just localStorage
+  const [loading, setLoading] = useState(true);
 
+  // grab user from database with token as ID
+  async function getUser(token) {
+    try {
+      const response = await axios.get("/api/users", {
+        headers: {
+          Authorization: token,
+        },
+      });
+      setUser(response.data);
+    } catch (err) {
+      console.log(err);
+      localStorage.removeItem("token");
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    // look for token in localstorage if we are logged in.
+    const token = localStorage.getItem("token");
+    if (token) {
+      // get user info, which is just token : asdfasdf right now in localstorage
+      getUser(token);
+    } else {
+      setLoading(false);
+    }
+  }, []);
+
+  let loggedIn = user.username;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <Navbar username={user.username} setUser={setUser} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {loggedIn ? (
+          <>
+            {/* if logged in */}
+            <Route path="/profile" element={<Profile username={user.username} email={user.email} />} />
+            {/* this will actually render for a sec, then redirect. Fixed to cond. load if we are not loading  */}
+            {!loading && <Route path="/login" element={<Navigate to="/" />} />}
+          </>
+        ) : (
+          <>
+            {/* if not logged in */}
+            <Route path="/login" element={<Login setUser={setUser} />} />
+            <Route path="/register" element={<Register setUser={setUser} />} />
+            {/* wild card * meaning anything thats not above, get this element = Navigate is like a redirect, not link  */}
+            {/* can be path=* or path=/profile  one excludes all others not name, the other is just /profile locked down */}
+
+            {!loading && (
+              <Route path="/profile" element={<Navigate to="/login" />} />
+            )}
+          </>
+        )}
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
