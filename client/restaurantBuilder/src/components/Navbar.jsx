9f246a21e0e2bf2eb010bbd76f29f87c18f02c22
Cristian <cristian91493@gmail.com>
2024-02-01 03:11:18 -0500

    actually finished the styling for homepage. made larger nav links and nav styling

--- client/restaurantBuilder/src/components/Navbar.jsx
+++ client/restaurantBuilder/src/components/Navbar.jsx
@@ -12,42 +12,41 @@ function Navbar({ username, setUser }) {
 
   return (
     <nav className="nav-bar">
-    <ul>
-      <li>
-        <Link to="/">
-          <img src={icon} alt="React Icon" id="icon" />
-        </Link>
-      </li>
-      {username ? 
-        <>
-          <li style={{ color: "black" }}>Welcome {username}!</li>
-          <li>
-            <Link to="/profile">Profile</Link>
-          </li>
-          <li onClick={logout}>
-            <Link to="/login">Logout</Link>
-          </li>
-          <li>
-            <Link to="/seeReservations">See Reservations</Link>
-          </li>
-        </>
-       : 
-        <>
-          <li className="nav-li">
-            <Link to="/reservations">Reservations</Link>
-          </li>
-          <li className="nav-li">
-            <Link to="/menu">Menu</Link>
-          </li>
-          <li className="nav-li">
-            <Link to="/aboutus">About Us</Link>
-          </li>
+      <ul>
+        <li>
+          <Link to="/">
+            <img src={icon} alt="React Icon" id="icon" />
+          </Link>
+        </li>
+        {username ? (
+          <>
+            <li style={{ color: "black" }}>Welcome {username}!</li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li onClick={logout}>
+              <Link to="/login">Logout</Link>
+            </li>
+            <li>
+              <Link to="/seeReservations">See Reservations</Link>
+            </li>
+          </>
+        ) : (
+          <>
+            <Link to="/reservations">
+              <li className="nav-li">Reservations</li>
+            </Link>
 
+            <Link to="/menu">
+              <li className="nav-li">Menu</li>
+            </Link>
 
-
-        </>
-      }
-    </ul>
+            <Link to="/aboutus">
+              <li className="nav-li">About Us</li>
+            </Link>
+          </>
+        )}
+      </ul>
     </nav>
   );
 }
--- client/restaurantBuilder/src/pages/Home.jsx
+++ client/restaurantBuilder/src/pages/Home.jsx
@@ -1,19 +1,17 @@
 function Home() {
-    return ( 
-        <div className="App">
-          
+  return (
+    <div className="App">
+      <div className="home-bg">
+        <div className="stacked-container">
+          <p className="res-name">CELESTE</p>
 
-                <div className="home-bg">
-<div className="button-container">
-
-                    <button className="home-res">Reserve</button>
-</div>
-
-                </div>
-
-            
+          <div className="button-container">
+            <button className="home-res">Reserve</button>
+          </div>
         </div>
-     );
+      </div>
+    </div>
+  );
 }
 
 export default Home;
