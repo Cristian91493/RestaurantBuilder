a255f7c34a7b30dbbb0f705ef8e25da534a6e371
Cristian <cristian91493@gmail.com>
2024-02-06 16:06:21 -0500

    uncommented vitconfig

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,9 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // proxy: {
-  //   '/auth': 'http://localhost:8080/',
-  //   '/api': 'http://localhost:8080/'
-  //   }
-  // },
-})
+  proxy: {
+    '/auth': 'http://localhost:8080/',
+    '/api': 'http://localhost:8080/'
+    }
+  },
+)
